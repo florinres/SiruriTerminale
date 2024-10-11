063b2d1eececa759e8e764376f404155f76f8af5
nxg06737 <raresbolca@gmail.com>
2024-10-11 16:04:19 +0300

    code changes, now multiple values can be inserted in a key

--- com/siruriterminale/Program.java
+++ com/siruriterminale/Program.java
@@ -4,21 +4,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Vector;
 
 class TerminalStrings{
 	private final String COMP = ">";
-	private final String DPUNCTE = ":";
 	private Vector<String> T = new Vector<String>();
 	private Vector<String> N = new Vector<String>();
 	private Vector<String> S = new Vector<String>();
-	private Vector<String> P = new Vector<String>();
+	private Map<String, Vector<String>> P = new HashMap<String, Vector<String>>();
+	private String checkSequence = new String();
+	int contor =0;
 	
 	TerminalStrings(){
 		try {
-			File f = new File("C:\\Users\\nxg06737\\eclipse-workspace-new\\SiruriTerminale\\com\\siruriterminale\\multimi.txt");
+			File f = new File("C:\\Users\\nxg06737\\OneDrive - NXP\\Desktop\\student_stuff\\SiruriTerminale\\com\\siruriterminale\\multimi.txt");
 			Scanner reader = new Scanner(f);
 			int counter = 0;
 			while(reader.hasNextLine()) {
@@ -46,8 +53,18 @@ class TerminalStrings{
 						break;
 					}
 					default:
-						P.add(String.valueOf(line.charAt(0)));
-						P.add(line.substring(line.indexOf(COMP) + 1, line.length()));
+						while(reader.hasNextLine()) {
+							String key = (String.valueOf(line.charAt(0)));
+							String object = (line.substring(line.indexOf(COMP) + 1, line.length()));
+							Vector<String> a = new Vector<String>();
+							if(P.containsKey(key)) {
+								a.addAll(P.get(key));
+							}
+							a.add(object);
+							P.put(key, a);
+							line = reader.nextLine();
+						}
+						break;
 					}
 				counter++;
 			}
@@ -58,70 +75,37 @@ class TerminalStrings{
 		}
 	}
 	
-	public String resultOfProduct(String a) {
-		for (int i = 0; i < P.size(); i++) {
-			String comp = P.get(i);
-			if(a.equals(comp)){
-				return P.get(i+1);
-			}
+	public void startProduction() {
+		String termOfStart = S.firstElement();
+		
+		while(!productionNotReady(termOfStart)) {
+			Vector<String> a = P.get(termOfStart);
 		}
-
-		return null;
 	}
 	
-	public String startProduction(String termenNeterminal) {
-		if(!P.contains(termenNeterminal)) {
-			System.out.println("Productia ceruta nu exista");
-			return null;
-		}
-		String sirNou = resultOfProduct(termenNeterminal);
-		continuaProduct(sirNou);
-		
-		return "";
-	}
-	
-	public void continuaProduct(String sirNou) {
-		String sirProdus = new String();
-		
-		if(verifiyIfTerminal(sirNou)) {
-			System.out.println("Sir este terminal");
-			return;
-		};
-		System.out.println("Sir neterminal");
-
-		//creare sir nou
-		for(int i = 0; i < sirNou.length();i++) {
-			String a = String.valueOf(sirNou.charAt(i));
-			String product = resultOfProduct(a);
-		}
-		
-	}
-
-	private boolean verifiyIfTerminal(String newString) {
-		Boolean []terminalStringBoolArray = new Boolean[newString.length()];
-		Arrays.fill(terminalStringBoolArray, false);
-		
-		//verificare daca sirul contine doar termeni terminali
-		for (int i = 0; i < newString.length(); i++) {
-			String a = String.valueOf(newString.charAt(i));
-			for (int j = 0; j < T.size(); j++) {
-				if(a.equals(T.get(j))) {
-					terminalStringBoolArray[i] = true;
-				}
+	//scans for non-terminal terms
+	public Boolean productionNotReady(String sequence) {
+		for(int i = 0; i < sequence.length(); i++) {
+			String compare = String.valueOf(sequence.charAt(i));
+			if(N.contains(compare)) {
+				return false;
 			}
-		}
-		for(Boolean b : terminalStringBoolArray) {
-			if(b == false) { return false; }
 		}
 		return true;
 	}
-}
+
+	public Boolean notTheSame(String sir) {
+		if (sir.equals(checkSequence)) {
+			return false;
+		}
+		
+		checkSequence = sir;
+		return true;
+	}
 
 public class Program {
 	public static void main(String[] args) {
 		TerminalStrings sir = new TerminalStrings();
-		sir.startProduction("S");
-		
+		sir.startProduction();
 	}
-
 }
